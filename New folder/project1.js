62f8ad0ffce60e35051d29ff991057c739e026e2
jerroldtkl <jerroldtkl@gmail.com>
2021-08-20 10:54:55 +0800

    project1 mvp complete

--- New folder/project1.js
+++ New folder/project1.js
@@ -1,18 +1,61 @@
+const modes = document.querySelector('.mode')
 const normal = document.querySelector('#normal')
 const realistic = document.querySelector('#realistic')
 
-//create a circle
-// function draw(){
-//   const circle = document.createElement('circle')
-//   const ctx = circle.getContext('2d')
-//   ctx.beginPath();
-//   ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-//   ctx.stroke();
-// }
+//create a timer
+let time = 15
+const timer = document.createElement('div')
+timer.setAttribute('class', 'timer')
+function countDown() {
+  timer.innerText = time + " second" + (time == 1 ? "" : "s")
+  if (time-- > 0) setTimeout(countDown, 1000) 
+  // console.log(time)
+};
+
+//create a function for setTimeout
+function gameOver(){
+    const score = document.createElement('div')
+    score.setAttribute('class', 'game-over')
+    // box.appendChild(gameOver)
+    // score.innerText = 'game over'
+    console.log('game over')
+    modes.appendChild(score)
+    alert('Game Over!')
+    return score
+}
+// create and append a div for points
+const pointsDiv = document.createElement('div')
+pointsDiv.setAttribute('class' , 'points')
+
+// create and append a div for accuracy
+//create and append accuracy
+const accuracyScore = document.createElement('div')
+accuracyScore.setAttribute('class', 'accuracy')
+
+//create an element points and click
+let points = 0
+let click = -1
+//calculate accuracy
+// let accuracy = 0
+// let accuracy = (points / click) * 100
+// console.log(accuracy + '%')
+
+function playMusic(){
+  document.querySelector('#background_music').play()
+  document.querySelector('#background_music').volume = 0.1
+}
+function pauseMusic(){
+  document.querySelector('#background_music').pause() 
+}
+//create a pop sound
+function popSound(){
+  const pop = document.createElement('audio')
+  pop.setAttribute('src', './media/sfx-pop.mp3')
+  pop.play()
+}
 
 function normalMode(){
   console.log('normal mode selected')
-  realistic.remove()
   //create a div for the 3 difficulties and a ul in it
   const threeLevels = document.createElement('div')
   const difficultyList = document.createElement('ul')
@@ -32,18 +75,19 @@ function normalMode(){
   threeLevels.appendChild(difficultyList)
   normal.appendChild(threeLevels)
 
-  easyLevel.addEventListener('click', function(){
+    easyLevel.addEventListener('click', function(){
     //clear innerHTML and make a new div to start the easy stage
-    document.body.innerHTML = ''
+    normal.innerHTML = ''
     const easyStage = document.createElement('div')
     easyStage.innerText = 'EASY'  
-    document.body.appendChild(easyStage)
+    normal.appendChild(easyStage)
     easyStage.style.color = 'yellow'
     easyStage.style.textAlign = 'center'
     easyStage.style.fontSize = '300%'
     // create a box in the center to show where the game will be played in
     const box = document.createElement('div')
-    document.body.appendChild(box)
+    normal.appendChild(box)
+    box.style.position = 'relative'
     box.style.backgroundColor = 'red'
     box.style.margin = 'auto'
     box.style.width = '600px'
@@ -52,17 +96,106 @@ function normalMode(){
     const start = document.createElement('button')
     start.innerText = 'START'
     box.appendChild(start)
+
+    //create a new div within the box
+    const circleDiv = document.createElement('div')
+    circleDiv.setAttribute('class', 'container')
+    box.appendChild(circleDiv)
+
+    //create a ul to store the list of circles
+    const ul = document.createElement('ul')
+
+    //start the game
     start.addEventListener('click', function(){
-      console.log('START')
-      start.innerText = 'STOP'
-      // randomize an image within the box
-      // function init(){
-        const random = document.querySelector('h1')
-        random.innerText = 'random'
-      // }
+    console.log('START')
+    start.innerText = 'Menu'
+    
+      // if the menu button is clicked
+        start.addEventListener('click', function(){
+        window.location.reload()
+        })
+
+        //run the timer
+        timer.innerText = time + " second" + (time == 1 ? "" : "s")
+        box.appendChild(timer)
+        countDown()
+        setTimeout(gameOver, 15200)
+        
+        // create and randomize circle within the box
+        function createCircle(){
+        const circle = document.createElement('button')
+        circle.setAttribute('class', 'dotEasy')
+        function randomWidth(min, max){
+          return Math.random() * (max - min) + min
+        }
+        
+        function randomHeight(min, max){
+          return Math.random() * (max - min) + min
+        }
+
+        circle.style.position = 'absolute'
+        circle.style.top = randomHeight(0,80) + '%'
+        circle.style.left = randomWidth(0,85) + '%';
+
+        //create a list to add the circles into the ul
+        const li = document.createElement('li')
+        li.appendChild(circle)
+        ul.appendChild(circle)
+        circleDiv.prepend(ul)
+        
+        
+        //create event listener for creating more circles
+          circle.addEventListener('click', popSound)
+          circle.addEventListener('click', createCircle)
+          circleDiv.firstChild.replaceChild(circleDiv.firstChild.childNodes[1],circleDiv.firstChild.childNodes[0])
+          //to record circleclicks
+          points++
+          console.log(points + 'points')
+       
+        // append points
+        pointsDiv.innerText = points + 'points'
+        box.appendChild(pointsDiv)
+
+        setTimeout(stopCircle, 2000)
+          function stopCircle(){
+          circle.removeEventListener('click', createCircle)
+          }
+          setTimeout(stopCircle, 2000)
+        }
+
+        //create a function to record clicks
+        function boxClick(){
+          click++
+          console.log(click + 'click')
+          let accuracy = (points / click) * 100
+          let accuracyRounded = accuracy.toFixed(2)
+          console.log(accuracy + '%')
+
+          // create a timer to stop all clicks after timer runs out
+          function stopClick(){
+          box.removeEventListener('click', boxClick)
+          }
+          setTimeout(stopClick, 15050)
+
+          //append accuracy
+          accuracyScore.innerText = 'accuracy =' + accuracyRounded + '%'
+          box.appendChild(accuracyScore)
+        }
+
+        box.addEventListener('click', boxClick)
+        createCircle()
+        
+        //append points
+        pointsDiv.innerText = points + 'points'
+        box.appendChild(pointsDiv)
+        
+        //append accuracy
+        accuracyScore.innerText = accuracyRounded
+        box.appendChild(accuracyScore)
     })
   })
-  }
+}
+
   function genLevelsMedium(){
   //create a li to insert into the ul
   const mediumList = document.createElement('li')
@@ -79,30 +212,125 @@ function normalMode(){
 
    mediumLevel.addEventListener('click', function(){
     //clear innerHTML and make a new div to start the easy stage
-    document.body.innerHTML = ''
-    const easyStage = document.createElement('div')
-    easyStage.innerText = 'MEDIUM'  
-    document.body.appendChild(easyStage)
-    easyStage.style.color = 'yellow'
-    easyStage.style.textAlign = 'center'
-    easyStage.style.fontSize = '300%'
+    normal.innerHTML = ''
+    const mediumStage = document.createElement('div')
+    mediumStage.innerText = 'MEDIUM'  
+    normal.appendChild(mediumStage)
+    mediumStage.style.color = 'yellow'
+    mediumStage.style.textAlign = 'center'
+    mediumStage.style.fontSize = '300%'
     // create a box in the center to show where the game will be played in
     const box = document.createElement('div')
-    document.body.appendChild(box)
+    normal.appendChild(box)
+    box.style.position = 'relative'
     box.style.backgroundColor = 'red'
     box.style.margin = 'auto'
-    box.style.width = '700px'
-    box.style.height = '500px'
+    box.style.width = '600px'
+    box.style.height = '400px'
     // create a button so start
     const start = document.createElement('button')
     start.innerText = 'START'
     box.appendChild(start)
+
+    //create a new div within the box
+    const circleDiv = document.createElement('div')
+    circleDiv.setAttribute('class', 'container')
+    box.appendChild(circleDiv)
+
+    //create a ul to store the list of circles
+    const ul = document.createElement('ul')
+
+    //start the game
     start.addEventListener('click', function(){
-      console.log('START')
-      start.innerText = 'STOP'
+    console.log('START')
+    start.innerText = 'Menu'
+
+      // if the menu button is clicked
+        start.addEventListener('click', function(){
+        window.location.reload()
+        })
+      
+        //run the timer
+        timer.innerText = time + " second" + (time == 1 ? "" : "s")
+        box.appendChild(timer)
+        countDown()
+        setTimeout(gameOver, 15200)
+
+        // create and randomize circle within the box
+        function createCircle(){
+        const circle = document.createElement('button')
+        circle.setAttribute('class', 'dotMedium')
+        function randomWidth(min, max){
+        return Math.random() * (max - min) + min
+        //width will be from left 171-771
+        }
+        function randomHeight(min, max){
+        // height will be from top 55 - 400
+        return Math.random() * (max - min) + min
+        }
+
+        circle.style.position = 'absolute'
+        circle.style.top = randomHeight(0,80) + '%'
+        circle.style.left = randomWidth(0,85) + '%';
+
+        //create a list to add the circles into the ul
+        const li = document.createElement('li')
+        li.appendChild(circle)
+        ul.appendChild(circle)
+        circleDiv.prepend(ul)
+      
+        //create event listener for creating more circles
+          circle.addEventListener('click', popSound)
+          circle.addEventListener('click', createCircle)
+          circleDiv.firstChild.replaceChild(circleDiv.firstChild.childNodes[1],circleDiv.firstChild.childNodes[0])
+          //to record circleclicks
+          points++
+          console.log(points + 'points')
+
+        // append points
+        pointsDiv.innerText = points + 'points'
+        box.appendChild(pointsDiv)
+
+        setTimeout(stopCircle, 2000)
+          function stopCircle(){
+          circle.removeEventListener('click', createCircle)
+          }
+          setTimeout(stopCircle, 2000)
+        }
+
+        //create a function to record clicks
+        function boxClick(){
+          click++
+          console.log(click + 'click')
+          let accuracy = (points / click) * 100
+          let accuracyRounded = accuracy.toFixed(2)
+          console.log(accuracy + '%')
+
+          // create a timer to stop all clicks after timer runs out
+          function stopClick(){
+          box.removeEventListener('click', boxClick)
+          }
+          setTimeout(stopClick, 15050)
+
+          //append accuracy
+          accuracyScore.innerText = accuracyRounded + '%'
+          box.appendChild(accuracyScore)
+        }
+
+        box.addEventListener('click', boxClick)
+        createCircle()
+        
+        //append points
+        pointsDiv.innerText = points + 'points'
+        box.appendChild(pointsDiv)
+        
+        //append accuracy
+        accuracyScore.innerText = accuracyRounded
+        box.appendChild(accuracyScore)
+      })
     })
-  })
   }
+
   function genLevelsHard(){
   //create a li to insert into the ul
   const hardList = document.createElement('li')
@@ -116,31 +344,126 @@ function normalMode(){
   difficultyList.appendChild(hardList)
   threeLevels.appendChild(difficultyList)
   normal.appendChild(threeLevels)
-  hardLevel.addEventListener('click', function(){
-    //clear innerHTML and make a new div to start the easy stage
-    document.body.innerHTML = ''
-    const hardStage = document.createElement('div')
-    hardStage.innerText = 'HARD'  
-    document.body.appendChild(hardStage)
-    hardStage.style.color = 'yellow'
-    hardStage.style.textAlign = 'center'
-    hardStage.style.fontSize = '300%'
-    // create a box in the center to show where the game will be played in
-    const box = document.createElement('div')
-    document.body.appendChild(box)
-    box.style.backgroundColor = 'red'
-    box.style.margin = 'auto'
-    box.style.width = '800px'
-    box.style.height = '600px'
-    // create a button so start
-    const start = document.createElement('button')
-    start.innerText = 'START'
-    box.appendChild(start)
-    start.addEventListener('click', function(){
+
+    hardLevel.addEventListener('click', function(){
+      //clear innerHTML and make a new div to start the easy stage
+      normal.innerHTML = ''
+      const hardStage = document.createElement('div')
+      hardStage.innerText = 'HARD'  
+      normal.appendChild(hardStage)
+      hardStage.style.color = 'yellow'
+      hardStage.style.textAlign = 'center'
+      hardStage.style.fontSize = '300%'
+      // create a box in the center to show where the game will be played in
+      const box = document.createElement('div')
+      normal.appendChild(box)
+      box.style.position = 'relative'
+      box.style.backgroundColor = 'red'
+      box.style.margin = 'auto'
+      box.style.width = '600px'
+      box.style.height = '400px'
+      // create a button so start
+      const start = document.createElement('button')
+      start.innerText = 'START'
+      box.appendChild(start)
+
+      //create a new div within the box
+      const circleDiv = document.createElement('div')
+      circleDiv.setAttribute('class', 'container')
+      box.appendChild(circleDiv)
+
+      //create a ul to store the list of circles
+      const ul = document.createElement('ul')
+
+      //start the game
+      start.addEventListener('click', function(){
       console.log('START')
-      start.innerText = 'STOP'
+      start.innerText = 'Menu'
+
+        // if the menu button is clicked
+          start.addEventListener('click', function(){
+          window.location.reload()
+          }) 
+
+          //run the timer
+          timer.innerText = time + " second" + (time == 1 ? "" : "s")
+          box.appendChild(timer)
+          countDown()
+          setTimeout(gameOver, 15200)
+
+          // create and randomize circle within the box
+          function createCircle(){
+          const circle = document.createElement('button')
+          circle.setAttribute('class', 'dotHard')
+          function randomWidth(min, max){
+            return Math.random() * (max - min) + min
+          }
+
+          function randomHeight(min, max){
+            return Math.random() * (max - min) + min
+          }
+          
+          circle.style.position = 'absolute'
+          circle.style.top = randomHeight(0,80) + '%'
+          circle.style.left = randomWidth(0,85) + '%';
+
+
+          //create a list to add the circles into the ul
+          const li = document.createElement('li')
+          li.appendChild(circle)
+          ul.appendChild(circle)
+          circleDiv.prepend(ul)
+        
+          //create event listener for creating more circles
+            circle.addEventListener('click', popSound)
+            circle.addEventListener('click', createCircle)
+            circleDiv.firstChild.replaceChild(circleDiv.firstChild.childNodes[1],circleDiv.firstChild.childNodes[0])
+            //to record circleclicks
+            points++
+            console.log(points + 'points')
+
+          // append points
+          pointsDiv.innerText = points + 'points'
+          box.appendChild(pointsDiv)
+          
+        setTimeout(stopCircle, 2000)
+          function stopCircle(){
+          circle.removeEventListener('click', createCircle)
+          }
+          setTimeout(stopCircle, 2000)
+        }
+
+        //create a function to record clicks
+        function boxClick(){
+          click++
+          console.log(click + 'click')
+          let accuracy = (points / click) * 100
+          let accuracyRounded = accuracy.toFixed(2)
+          console.log(accuracy + '%')
+
+          // create a timer to stop all clicks after timer runs out
+          function stopClick(){
+          box.removeEventListener('click', boxClick)
+          }
+          setTimeout(stopClick, 15050)
+
+          //append accuracy
+          accuracyScore.innerText = 'accuracy =' + accuracyRounded + '%'
+          box.appendChild(accuracyScore)
+        }
+
+        box.addEventListener('click', boxClick)
+        createCircle()
+        
+        //append points
+        pointsDiv.innerText = points + 'points'
+        box.appendChild(pointsDiv)
+        
+        //append accuracy
+        accuracyScore.innerText = accuracyRounded
+        box.appendChild(accuracyScore)
+      })
     })
-  })
   }
   genLevelsEasy()
   genLevelsMedium()
@@ -150,5 +473,14 @@ function normalMode(){
 }
 
 normal.addEventListener('click', normalMode)
-
-
+document.querySelector('.music_off').addEventListener('click', function(e){
+  if(document.querySelector('#background_music').muted === true){
+    playMusic()
+    e.target.src = './image/volume.jpg'
+    document.querySelector('#background_music').muted = false
+  } else {
+  pauseMusic()
+  e.target.src = "./image/muted.jpg"
+  document.querySelector('#background_music').muted = true    
+  }
+})
--- circle generate/circle.js
+++ circle generate/circle.js
@@ -1,3 +1,21 @@
-const circle = document.createElement('dot')
-circle.setAttribute('class', 'dot')
-document.body.appendChild(circle)
+const body = document.querySelector('body')
+const dot = document.querySelector('.dot')
+body.addEventListener('click', function(e){
+  click ++
+  console.log(click + ' times clicked')
+
+  if(e.target === dot){
+    dotclick++
+    console.log(dotclick + ' dot clicked')
+  }
+  let percentage = ((dotclick / click) * 100)
+  let percentage2 = percentage.toFixed(2)
+  console.log(percentage + '%')
+  console.log(percentage2)
+})
+
+let click = 0
+let dotclick = 0
+
+// let percentage = ((dotclick / click) * 100)
+// console.log(percentage)
